a6203e73a7e6f7ed84006d7c5644dbff2509d257
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-13 00:26:24 +0530

    clerk provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +11,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <main className="conatiner">
+            <div className="flex items-start justify-center min-h-screen">
+              <div className="mt-20">
+                {children}
+              </div>
+            </div>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+
   )
 }
