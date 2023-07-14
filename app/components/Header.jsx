6bf07f48a9dbbee6e53280913aad88718a980d18
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-14 13:07:11 +0530

    header conditials and user bnutton component

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,5 +1,10 @@
 import Link from "next/link"
+import { auth, UserButton } from "@clerk/nextjs"
+
 const Header = () => {
+    const { userId } = auth();
+    console.log(userId); //note clg will be down in terminal not browseer as default on server noit client
+
     return (
         <>
             <nav className="bg-blue-700 py-4 px-6 flex items-center justify-between mb-5">
@@ -10,13 +15,20 @@ const Header = () => {
                         </div>
                     </Link>
                 </div>
-                <div className="text-white">
-                    <Link href="sign-in" className="text-gray-300 hover:text-white">
-                        Sign In
-                    </Link>
-                    <Link href="sign-up" className="text-gray-300 hover:text-white">
-                        Sign Up
-                    </Link>
+                <div className="text-white flex items-center">
+                    {!userId && (
+                        <>
+                            <Link href="sign-in" className="text-gray-300 hover:text-white">
+                                Sign In
+                            </Link>
+                            <Link href="sign-up" className="text-gray-300 hover:text-white">
+                                Sign Up
+                            </Link>
+                        </>
+                    )}
+                    <div className="ml-auto">
+                        <UserButton afterSignOutUrl='/' />
+                    </div>
                 </div>
             </nav>
         </>
